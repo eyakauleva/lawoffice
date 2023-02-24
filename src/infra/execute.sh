c53b5f4bd6b82db0ffa121f1644b69ca96bc0829
eyakauleva <eyakovleva.laba@solvd.com>
2023-02-24 15:39:20 +0300

    url environment variable fix in k8s files

--- src/infra/execute.sh
+++ src/infra/execute.sh
@@ -5,6 +5,7 @@ kubectl apply -f app-service.yaml
 kubectl apply -f app-deployment.yaml
 
 kubectl apply -f pg-secrets.yaml
+kubectl apply -f pg-configmap.yaml
 kubectl apply -f pg-service.yaml
 kubectl apply -f pg-stateful.yaml
 
